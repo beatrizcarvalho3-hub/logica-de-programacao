045dfa8a8534221e0f2eb449e519838f8ad38e77
bebela <beatriz.carvalho3@eaportal.org>
2026-08-14 09:51:07 -0300

    Add files via upload
    atividade do dia 14/08

---
+++ ATIVIDADE PYTHON/atividade.py
@@ -0,0 +1,7 @@
+placa = input("Digite a placa do veículo (ex: ABC1D23): ")
+
+letras_iniciais = placa[0:3]
+sufixo = placa[3:]
+
+print(f"Letras iniciais: {letras_iniciais}")
+print(f"Sufixo da placa: {sufixo}")
